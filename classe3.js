86cbac1177d095e32b683f9eac2911333b19b817
silviaGenia <wayra204genia@gmail.com>
2023-06-27 10:45:13 -0400

    fix: variables: number

---
+++ classe3.js
@@ -0,0 +1,26 @@
+let edad = 27;
+//incremento
+//edad = edad + 1;
+//o lo escribes ese incremento
+edad += 1;
+
+let peso = 75;
+//peso = peso - 2;
+//o lo escribes asi
+peso -= 2;
+
+var torta = 1;
+peso = peso + torta;
+
+let jugar = 3;
+peso = peso - jugar;
+
+let vino = 20.3;
+//para que no ocurra ese error en los decimales de debe multiplicar por 100 y dividir por 100
+let total = (vino * 100 * 3) / 100;
+let totall = Math.round(vino * 3);
+let total1 = vino * 3;
+//redondear con los decimales
+let totalstr = total.toFixed(3);
+//para cnvertir un float de string a entero
+var total2 = parseFloat(totalstr);
