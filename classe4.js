7e57e3d69759fb949ff10b0f7f7952cce142238f
silviaGenia <wayra204genia@gmail.com>
2023-06-27 11:15:04 -0400

    fix: funciones

---
+++ classe4.js
@@ -0,0 +1,13 @@
+//Las funciones son pedasos de codigos reutilisable
+let nombre = "keyla",
+  edad = 29;
+
+/* function imprimirEdad() {
+  console.log(`${nombre} tiene ${edad} años`);
+}
+ */
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`);
+}
+imprimirEdad(nombre, edad);
