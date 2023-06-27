3c61da74074042852a3c7cbb9d6719b80754954e
silviaGenia <wayra204genia@gmail.com>
2023-06-27 09:59:50 -0400

    fix: variables y string

---
+++ classe2.js
@@ -0,0 +1,20 @@
+let nombre = "Sacha";
+let apellido = "Lizfszyc";
+
+let nombreEnMyusculas = nombre.toUpperCase();
+let nombreEnMynusculas = apellido.toLowerCase();
+
+let primerLetraDelNombre = nombre.charAt(0);
+
+let cantidadDeLetrasDelNombre = nombre.length;
+
+//concatenar
+let nombreCompleto = nombre + "" + apellido;
+
+//concatenar lo nuevo
+let nombreCompleto1 = `${nombre} ${apellido.toUpperCase()}`;
+
+//
+let str1 = nombre.charAt(1) + nombre.charAt(2);
+
+let str = nombre.substr(1, 2);
